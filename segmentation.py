800d1f102a6821c05550ab79f012562d150d96d1
Aleksa Gordic <gordicaleksa@gmail.com>
2020-08-02 20:45:05 +0200

    Refactor segmentation logic

--- segmentation.py
+++ segmentation.py
@@ -1,89 +1,64 @@
 import argparse
+import subprocess
+import time
+import os
+import shutil
 
 
 from torchvision import models
 import torch
 from torchvision import datasets
 from torchvision import transforms
-from torch.utils.data import DataLoader, Sampler
-
-
-from PIL import Image
+from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-import subprocess
 import cv2 as cv
-import time
-import shutil
 
 
-SUPPORTED_VIDEOS = ['.mp4']
+SUPPORTED_VIDEO_EXTENSIONS = ['.mp4']
+
+
+FILE_NAME_NUM_DIGITS = 6  # number of digits in the frame/mask names, e.g. for 6: '000023.jpg'
 
 
 IMAGENET_MEAN_1 = np.array([0.485, 0.456, 0.406], dtype=np.float32)
 IMAGENET_STD_1 = np.array([0.229, 0.224, 0.225], dtype=np.float32)
 
 
-def mp4_filter(vid_name):
-    return vid_name.endswith('.mp4')
+def post_process_mask(mask):
+    # step1: morphological filtering (helps splitting parts that don't belong to the person blob)
+    kernel = np.ones((13, 13), np.uint8)  # hardcoded 13 simply gave nice results
+    opened_mask = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+
+    # step2: isolate the person component (biggest component after background)
+    num_labels, labels, stats, _ = cv.connectedComponentsWithStats(opened_mask)
+
+    # step2.1: find the background component
+    h, _ = labels.shape  # get mask height
+    # find the most common index in the upper 10% of the image - I consider that to be the background index (heuristic)
+    discriminant_subspace = labels[:int(h/10), :]
+    bkg_index = np.argmax(np.bincount(discriminant_subspace.flatten()))
+
+    # step2.2: biggest component after background is person (hypothesis)
+    blob_areas = []
+    for i in range(0, num_labels):
+        blob_areas.append(stats[i, cv.CC_STAT_AREA])
+    blob_areas = list(zip(range(len(blob_areas)), blob_areas))
+    blob_areas.sort(key=lambda tup: tup[1], reverse=True)  # sort from biggest to smallest area components
+    blob_areas = [a for a in blob_areas if a[0] != bkg_index]  # remove background component
+    person_index = blob_areas[0][0]  # biggest component that is not background is presumably person
+    processed_mask = np.uint8((labels == person_index) * 255)
+
+    return processed_mask
 
 
-def decode_segmap(image, nc=21):
-    label_colors = np.array([(0, 0, 0),  # 0=background
-                             # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
-                             (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
-                             # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
-                             (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
-                             # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
-                             (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
-                             # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
-                             (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
-
-    r = np.zeros_like(image).astype(np.uint8)
-    g = np.zeros_like(image).astype(np.uint8)
-    b = np.zeros_like(image).astype(np.uint8)
-
-    for l in range(0, nc):
-        idx = image == l
-        r[idx] = label_colors[l, 0]
-        g[idx] = label_colors[l, 1]
-        b[idx] = label_colors[l, 2]
-
-    rgb = np.stack([r, g, b], axis=2)
-    return rgb
-
-
-# img_path = r"C:\tmp_data_dir\person.jpeg"
-# dump_mask_single_img(dlab, path=img_path, show_orig=False)
-def dump_mask_single_img(net, path, show_orig=True, dev='cuda'):
-  img = Image.open(path)
-  if show_orig: plt.imshow(img); plt.axis('off'); plt.show()
-  # Comment the Resize and CenterCrop for better inference results
-  trf = transforms.Compose([T.Resize(640),
-                   #T.CenterCrop(224),
-                   T.ToTensor(),
-                   T.Normalize(mean=IMAGENET_MEAN_1,
-                               std=IMAGENET_STD_1)])
-  inp = trf(img).unsqueeze(0).to(dev)
-  out = net.to(dev)(inp)['out']
-  om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
-  # rgb = decode_segmap(om)
-  gray = om == 15
-  tmp = Image.fromarray(np.uint8(gray*255))
-  tmp.save(os.path.join(os.path.split(path)[0], 'mask.png'))
-
-  # plt.imshow(gray); plt.show()
-  # plt.imshow(tmp); plt.axis('off'); plt.show()
-
-
-def extract_masks_from_frames(model, device, processed_video_dir, frames_path, batch_size, format):
+def extract_masks_from_frames(model, device, processed_video_dir, frames_path, batch_size, mask_extension):
     masks_dump_path = os.path.join(processed_video_dir, 'masks')
     processed_masks_dump_path = os.path.join(processed_video_dir, 'processed_masks')
     os.makedirs(masks_dump_path, exist_ok=True)
     os.makedirs(processed_masks_dump_path, exist_ok=True)
 
-    HUMAN_CLASS = 15
+    PERSON_CHANNEL_INDEX = 15
     transform = transforms.Compose([
         transforms.Resize(540),
         transforms.ToTensor(),
@@ -94,62 +69,24 @@ def extract_masks_from_frames(model, device, processed_video_dir, frames_path, b
 
     with torch.no_grad():
         for batch_id, (img_batch, _) in enumerate(frames_loader):
-            img_batch = img_batch.to(device)
+            print(f'Processing batch {batch_id + 1}.')
+            img_batch = img_batch.to(device)  # shape: (N, 3, H, W)
+            result_batch = model(img_batch)['out'].to('cpu').numpy()  # shape: (N, 21, H, W) (21 - PASCAL VOC classes)
+            for j, out_cpu in enumerate(result_batch):
+                # When for the pixel position (x, y) the biggest (un-normalized) probability
+                # lies in the channel PERSON_CHANNEL_INDEX we set the mask pixel to True
+                mask = np.argmax(out_cpu, axis=0) == PERSON_CHANNEL_INDEX
+                mask = np.uint8(mask * 255)  # convert from bool to [0, 255] black & white image
 
-            out_cpu_batch = model(img_batch)['out'].to('cpu').numpy()
-            for j, out_cpu in enumerate(out_cpu_batch):
-                print('creating mask for frame #{}'.format(batch_id*batch_size+j))
-                mask = np.argmax(out_cpu, axis=0) == HUMAN_CLASS
+                processed_mask = post_process_mask(mask)  # simple heuristics (connected components, etc.)
 
-                mask = np.uint8(mask * 255)
-                processed_mask = process_mask(mask, batch_id*batch_size+j)
-
-                mask = Image.fromarray(mask)
-                processed_mask = Image.fromarray(processed_mask)
-                # print(np.histogram(processed_mask, bins=np.arange(256)))
-
-                mask_path = os.path.join(masks_dump_path, str(batch_id*batch_size+j).zfill(4) + '_mask' + format)
-                pmask_path = os.path.join(processed_masks_dump_path, str(batch_id*batch_size+j).zfill(4) + '_p_mask' + format)
-
-                mask.save(mask_path)
-                processed_mask.save(pmask_path)
-
-                # plt.imshow(mask)
-                # plt.show()
+                filename = str(batch_id*batch_size+j).zfill(FILE_NAME_NUM_DIGITS) + mask_extension
+                # ::-1 because opencv expects BGR (and not RGB) format...
+                cv.imwrite(os.path.join(masks_dump_path, filename), mask)
+                cv.imwrite(os.path.join(processed_masks_dump_path, filename), processed_mask)
 
 
-# https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
-def process_mask(mask, it):
-    # ts = time.time()
-    # step1: morphological filtering
-    kernel = np.ones((13, 13), np.uint8)
-    opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
-
-    # step2: isolate the human component
-    output = cv.connectedComponentsWithStats(opening)
-    num_labels = output[0]
-    labels = output[1]
-    h, _ = labels.shape
-    discriminant_subspace = labels[:int(h/10), :]
-    bkg_index = np.bincount(discriminant_subspace.flatten()).argmax()
-    stats = output[2]
-    areas = []
-    for i in range(0, num_labels):
-        areas.append(stats[i, cv.CC_STAT_AREA])
-    areas = list(zip(range(len(areas)), areas))
-    areas.sort(key=lambda tup: tup[1], reverse=True)
-    areas = [a for a in areas if a[0] != bkg_index]  # remove background component
-    human_index = areas[0][0]  # biggest component that is not background is presumably human
-    processed_mask = np.uint8((labels == human_index) * 255)
-
-    # print('processing took: {:.3f} ms'.format((time.time()-ts)*1000))
-
-    # plt.imshow(np.hstack([labels, processed_mask]))
-    # plt.show()
-
-    return processed_mask
-
-
+# todo: should I add fast NST as a submodule project?
 if __name__ == "__main__":
     #
     # Fixed args - don't change these unless you have a good reason
@@ -157,7 +94,7 @@ if __name__ == "__main__":
     # todo: figure out why is jpg giving me bad visual quality for dummy.mp4 video
     frame_extension = '.jpg'  # .jpg is suitable to use here - smaller size and unobservable quality loss
     mask_extension = '.png'  # don't use .jpg here! bigger size + corrupts the binary property of the mask when loaded
-    frame_name_format = '%06d' + frame_extension  # e.g. 000023.jpg
+    frame_name_format = f'%0{FILE_NAME_NUM_DIGITS}d{frame_extension}'  # e.g. 000023.jpg
     data_path = os.path.join(os.path.dirname(__file__), 'data')
 
     #
@@ -174,7 +111,7 @@ if __name__ == "__main__":
 
     for element in os.listdir(data_path):
         maybe_video_path = os.path.join(data_path, element)
-        if os.path.isfile(maybe_video_path) and os.path.splitext(maybe_video_path)[1].lower() in SUPPORTED_VIDEOS:
+        if os.path.isfile(maybe_video_path) and os.path.splitext(maybe_video_path)[1].lower() in SUPPORTED_VIDEO_EXTENSIONS:
             video_path = maybe_video_path
             video_name = os.path.basename(video_path).split('.')[0]
 
@@ -200,6 +137,6 @@ if __name__ == "__main__":
 
                 ts = time.time()
                 extract_masks_from_frames(segmentation_model, device, processed_video_dir, frames_path, args.segmentation_batch_size, mask_extension)
-                print(f'Time it took for masks {(time.time() - ts):.3f} [s].')
+                print(f'Time it took for computing masks {(time.time() - ts):.3f} [s].')
             else:
                 raise Exception(f'{ffmpeg} not found in the system path, aborting.')
