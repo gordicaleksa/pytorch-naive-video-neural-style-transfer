6f951a100d9e37f44a6526cbd8342a464ea74c5c
Aleksa Gordic <gordicaleksa@gmail.com>
2020-08-06 20:59:51 +0200

    Add better quality for jpg when dumping frames and remove ffmpeg branch

--- naive_video_pipeline.py
+++ naive_video_pipeline.py
@@ -22,7 +22,6 @@ if __name__ == "__main__":
     #
     # Fixed args - don't change these unless you have a good reason
     #
-    # todo: figure out why is jpg giving me bad visual quality for dummy.mp4 video
     frame_extension = '.jpg'  # .jpg is suitable to use here - smaller size and unobservable quality loss
     mask_extension = '.png'  # don't use .jpg here! bigger size + corrupts the binary property of the mask when loaded
     frame_name_format = f'%0{FILE_NAME_NUM_DIGITS}d{frame_extension}'  # e.g. 000023.jpg
@@ -33,6 +32,7 @@ if __name__ == "__main__":
     #
     # todo: refactor
     # todo: add readme
+    # todo: improve manual cleaning script
     parser = argparse.ArgumentParser()
     # segmentation stage params (these 2 help with GPU VRAM problems or you can try changing the segmentation model)
     parser.add_argument("--segmentation_batch_size", type=int, help="Number of images in a batch", default=12)
@@ -50,7 +50,9 @@ if __name__ == "__main__":
     nst_submodule_path = os.path.join(os.path.dirname(__file__), 'pytorch-nst-feedforward')
     assert os.path.exists(nst_submodule_path), 'Please pull the pytorch-nst-feedforward submodule to use this project.'
     model_path = os.path.join(nst_submodule_path, 'models', 'binaries', args.model_name)
-    assert os.path.exists(model_path), f'Could not find {model_path}. Make sure to first place the model in there.'
+    assert os.path.exists(model_path), f'Could not find {model_path}. Make sure to first manually place the model in there.'
+    ffmpeg = 'ffmpeg'
+    assert shutil.which(ffmpeg), f'{ffmpeg} not found in the system path. Please add it before running this script.'
 
     #
     # For every video located under data/ run this pipeline
@@ -68,60 +70,58 @@ if __name__ == "__main__":
             frames_path = os.path.join(processed_video_dir, 'frames', 'frames')
             os.makedirs(frames_path, exist_ok=True)
 
-            ffmpeg = 'ffmpeg'
-            if shutil.which(ffmpeg):  # if ffmpeg is in system path
-                cap = cv.VideoCapture(video_path)
-                fps = int(cap.get(cv.CAP_PROP_FPS))
 
-                out_frame_pattern = os.path.join(frames_path, frame_name_format)
-                audio_dump_path = os.path.join(processed_video_dir, video_name + '.aac')
+            cap = cv.VideoCapture(video_path)
+            fps = int(cap.get(cv.CAP_PROP_FPS))
 
-                #
-                # step1: Extract frames from the videos as well as audio file
-                #
-                if len(os.listdir(frames_path)) == 0:
-                    subprocess.call([ffmpeg, '-i', video_path, '-r', str(fps), '-start_number', '0', out_frame_pattern, '-c:a', 'copy', audio_dump_path])
-                else:
-                    print('Skip splitting video into frames and audio, already done.')
-                print('Stage 1/5 done (split video into frames and audio file).')
+            out_frame_pattern = os.path.join(frames_path, frame_name_format)
+            audio_dump_path = os.path.join(processed_video_dir, video_name + '.aac')
 
-                #
-                # step2: Extract person segmentation mask from frames
-                #
-                ts = time.time()
-                mask_dirs = extract_person_masks_from_frames(processed_video_dir, frames_path, args.segmentation_batch_size, args.segmentation_mask_width, mask_extension)
-                print('Stage 2/5 done (create person segmentation masks).')
-                print(f'Time elapsed computing masks: {(time.time() - ts):.3f} [s].')
-
-                #
-                # step3: Stylize dumped video frames
-                #
-                ts = time.time()
-                style_dir = stylization(frames_path, args.model_name, args.img_width)
-                print('Stage 3/5 done (stylize dumped video frames).')
-                print(f'Time elapsed stylizing imagery: {(time.time() - ts):.3f} [s].')
-
-                #
-                # step4: Combine stylized frames with masks
-                #
-                relevant_directories = {}
-                relevant_directories['frames_path'] = frames_path
-                relevant_directories.update(mask_dirs)
-                relevant_directories.update(style_dir)
-                relevant_directories['audio_path'] = audio_dump_path
-
-                ts = time.time()
-                combined_dirs = stylized_frames_mask_combiner(relevant_directories, frame_extension)
-                print('Stage 4/5 done (combine masks with stylized frames).')
-                print(f'Time elapsed masking stylized imagery: {(time.time() - ts):.3f} [s].')
-
-                #
-                # step5: Create videos
-                #
-                relevant_directories.update(combined_dirs)
-
-                video_metadata = {'fps': fps}
-                create_videos(video_metadata, relevant_directories, frame_name_format, args.delete_source_imagery)
-                print('Stage 5/5 done (create videos from overlayed stylized frames).')
+            #
+            # step1: Extract frames from the videos as well as audio file
+            #
+            if len(os.listdir(frames_path)) == 0:
+                subprocess.call([ffmpeg, '-i', video_path, '-r', str(fps), '-start_number', '0', '-qscale:v', '2', out_frame_pattern, '-c:a', 'copy', audio_dump_path])
             else:
-                raise Exception(f'{ffmpeg} not found in the system path, aborting.')
+                print('Skip splitting video into frames and audio, already done.')
+            print('Stage 1/5 done (split video into frames and audio file).')
+
+            #
+            # step2: Extract person segmentation mask from frames
+            #
+            ts = time.time()
+            mask_dirs = extract_person_masks_from_frames(processed_video_dir, frames_path, args.segmentation_batch_size, args.segmentation_mask_width, mask_extension)
+            print('Stage 2/5 done (create person segmentation masks).')
+            print(f'Time elapsed computing masks: {(time.time() - ts):.3f} [s].')
+
+            #
+            # step3: Stylize dumped video frames
+            #
+            ts = time.time()
+            style_dir = stylization(frames_path, args.model_name, args.img_width)
+            print('Stage 3/5 done (stylize dumped video frames).')
+            print(f'Time elapsed stylizing imagery: {(time.time() - ts):.3f} [s].')
+
+            #
+            # step4: Combine stylized frames with masks
+            #
+            relevant_directories = {}
+            relevant_directories['frames_path'] = frames_path
+            relevant_directories.update(mask_dirs)
+            relevant_directories.update(style_dir)
+            relevant_directories['audio_path'] = audio_dump_path
+
+            ts = time.time()
+            combined_dirs = stylized_frames_mask_combiner(relevant_directories, frame_extension)
+            print('Stage 4/5 done (combine masks with stylized frames).')
+            print(f'Time elapsed masking stylized imagery: {(time.time() - ts):.3f} [s].')
+
+            #
+            # step5: Create videos
+            #
+            relevant_directories.update(combined_dirs)
+
+            video_metadata = {'fps': fps}
+            create_videos(video_metadata, relevant_directories, frame_name_format, args.delete_source_imagery)
+            print('Stage 5/5 done (create videos from overlayed stylized frames).')
+
