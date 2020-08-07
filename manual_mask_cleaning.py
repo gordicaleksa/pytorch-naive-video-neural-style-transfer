2b411561e1d7233bb2763a335b9fb6cb78105be4
Aleksa Gordic <gordicaleksa@gmail.com>
2020-08-07 11:03:32 +0200

    Refactor manual mask cleaning

--- manual_mask_cleaning.py
+++ manual_mask_cleaning.py
@@ -1,48 +1,59 @@
+"""
+    Automatic segmentation is not always perfect, this scripts helps by providing semi-automatic cleaning.
+    Final possibility is using some editing software and editing masks there (high cost).
+
+    Usual workflow:
+        1. Copy processed_masks/ into processed_masks_refined/ (as this script has destructive process)
+        2. Manually inspect masks and find the range that can be filled/deleted with a rectangular/custom mask
+        3. Tweak the params in top of the main function and run (start in debug mode if you're not sure how it works)
+"""
+
 import os
-from PIL import Image
+import sys
+import enum
+# Enables this project to see packages from pytorch-nst-feedforward submodule (e.g. utils)
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'pytorch-nst-feedforward'))
+
+
+import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2 as cv
 
 
-if __name__=="__main__":
-    root = r'C:\tmp_data_dir\YouTube\Videos\NST_3_Optimization\camera_capture\intro_part_of_outro\intro_split\clip_1\processed_masks_refined' # r"C:\tmp_data_dir\YouTube\NST\Repos\examples\fast_neural_style\my_videos\NST_v2\clip_4\processed_masks_refined"
-    clear_mask_path = r"C:\tmp_data_dir\YouTube\NST\Repos\examples\fast_neural_style\my_videos\NST_v2\clip_4\clear_mask9.png"
+# Using functions from utils package from pytorch-nst-feedforward submodule like load_image
+from utils import utils
 
-    choice = 1
 
-    if choice == 2:
-        clear_mask = load_image(clear_mask_path)[:,:,0]
-    kernel = np.ones((13, 13), np.uint8)
+class Mode(enum.Enum):
+    RECTANGULAR = 0,
+    CUSTOM_MASK = 1
 
-    for cnt, img_name in enumerate(os.listdir(root)):
 
-        if choice == 1:
-            if 0 <= cnt <= 1041:
-                img_path = os.path.join(root, img_name)
-                img = load_image(img_path)
-                print(img.shape, np.max(img))
-                img[500:, 417:700] = 255
-                img = Image.fromarray(img)
-                img.save(img_path)
-        elif choice == 2:
-            if 1147 <= cnt <= 1237:
-                img_path = os.path.join(root, img_name)
-                img = load_image(img_path)
-                # plt.imshow(clear_mask == 255)
-                # plt.show()
-                img[clear_mask == 255] = 0
-                img = Image.fromarray(img)
-                img.save(img_path)
-        else:
-            img_path = os.path.join(root, img_name)
-            img = load_image(img_path)
-            eroded = cv.morphologyEx(img, cv.MORPH_ERODE, kernel)
-            # rgb = np.zeros((540, 960, 3))
-            # rgb[:,:,0] = img
-            # rgb[:,:,1] = eroded
-            # rgb[:,:,2] = eroded
-            # plt.imshow(rgb)
-            # plt.show()
-            eroded = Image.fromarray(eroded)
-            eroded.save(img_path)
+if __name__ == "__main__":
+    # place your paths here
+    mask_root = r'C:\tmp_data_dir\YouTube\CodingProjects\pytorch-naive-video-nst\data\clip_example\processed_masks_refined'
+    clear_mask_path = r"C:\tmp_data_dir\YouTube\CodingProjects\pytorch-naive-video-nst\data\clip_example\custom_mask.png"
+    mode = Mode.RECTANGULAR
+    FIRST_IMAGE_INDEX = 0  # specify the first image in the directory that should be processed
+    LAST_IMAGE_INDEX = 100  # and the last one (included)
+    should_delete = False  # rectangular region and custom mask both fill (make white) the specified pixels
+
+    if mode == Mode.CUSTOM_MASK:
+        clear_mask = utils.load_image(clear_mask_path)[:, :, 0]
+
+    for cnt, img_name in enumerate(os.listdir(mask_root)):
+        img_path = os.path.join(mask_root, img_name)
+        img = utils.load_image(img_path)[:, :, 0]
+
+        # if in correct range
+        if FIRST_IMAGE_INDEX <= cnt <= LAST_IMAGE_INDEX:
+            # step1: edit image
+            if mode == Mode.RECTANGULAR:
+                # manually specify rectangular region here
+                img[215:, 85:300] = 0. if should_delete else 1.
+            elif mode == Mode.CUSTOM_MASK:
+                img[clear_mask == 1.] = 0. if should_delete else 1.
+            else:
+                raise Exception(f'{mode} not supported.')
+            # step2: overwrite old image
+            cv.imwrite(img_path, np.uint8(img * 255))
